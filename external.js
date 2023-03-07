8f477ef2c45bbfd82d0eff950eb71381aeafda46
Brian <brianrondolo@yahoo.com>
2023-03-06 18:45:59 -0800

    play a round with computer

--- external.js
+++ external.js
@@ -6,3 +6,25 @@ function getComputerChoice() {
     return options[randNum];
 }
 
+function playRound(playerSelection, computerSelection) {
+    let options = ['rock', 'paper', 'scissors'];
+    if(playerSelection === computerSelection) {
+        console.log(`Player:${playerSelection} Computer:${computerSelection} It's a Tie!`);
+    }
+    else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection ==='paper')
+        ){
+        console.log(`Player:${playerSelection} Computer:${computerSelection} Player Wins!`);
+
+    }
+    else if (
+        (computerSelection === 'rock' && playerSelection === 'scissors') ||
+        (computerSelection === 'paper' && playerSelection === 'rock') ||
+        (computerSelection === 'scissors' && playerSelection ==='paper')
+        ){
+        console.log(`Player:${playerSelection} Computer:${computerSelection} Computer Wins!`);
+    }
+}
+
